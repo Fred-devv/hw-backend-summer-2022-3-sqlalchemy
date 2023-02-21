96ee343c889ea53a85f91626e0933fd0e0ac97fb
Fedor Kuhta <fedorkuhta@yandex.ru>
2023-02-21 15:25:45 +0300

    All Done_13

--- app/store/database/database.py
+++ app/store/database/database.py
@@ -7,23 +7,20 @@ from app.store.database import db
 if TYPE_CHECKING:
     from app.web.app import Application
 
-#url для локальной работы
-#url = "postgresql+asyncpg://postgres:762063@localhost/victorina"
-#url для тестов
-url = "postgresql+asyncpg://postgres:meddyson888@localhost/kts"
 
 class Database:
     def __init__(self, app: "Application"):
         self.app = app
         self._engine: Optional[AsyncEngine] = None
         self._db: Optional[declarative_base] = None
-        self.session: Optional[AsyncSession] = None
+        self.session: Optional[sessionmaker] = None
 
     async def connect(self, *_: list, **__: dict) -> None:
         self._db = db
-        self._engine = create_async_engine(url, echo=True, future=False)
-        self.session = sessionmaker(self._engine, expire_on_commit=False, future=True, class_=AsyncSession)
+        self._engine = create_async_engine(self.app.config.database.url, echo=True, future=True)
+        self.session = sessionmaker(self._engine, expire_on_commit=False, class_=AsyncSession)
 
     async def disconnect(self, *_: list, **__: dict) -> None:
         if self._engine:
             await self._engine.dispose()
+
