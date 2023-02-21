4086c8b0c13f6935dcc118718d702050990cf70e
Fedor Kuhta <fedorkuhta@yandex.ru>
2023-02-21 13:57:46 +0300

    All Done

--- app/store/database/database.py
+++ app/store/database/database.py
@@ -1,12 +1,16 @@
 from typing import Optional, TYPE_CHECKING
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.store.database import db
 
 if TYPE_CHECKING:
     from app.web.app import Application
 
+#url для локальной работы
+#url = "postgresql+asyncpg://postgres:762063@localhost/victorina"
+#url для тестов
+url = "postgresql+asyncpg://postgres:meddyson888@localhost/kts"
 
 class Database:
     def __init__(self, app: "Application"):
@@ -17,8 +21,9 @@ class Database:
 
     async def connect(self, *_: list, **__: dict) -> None:
         self._db = db
-        # self._engine = create_async_engine()
-        # self.session = sessionmaker()
+        self._engine = create_async_engine(url, echo=True, future=False)
+        self.session = sessionmaker(self._engine, expire_on_commit=False, future=True, class_=AsyncSession)
 
     async def disconnect(self, *_: list, **__: dict) -> None:
-        raise NotImplemented
+        if self._engine:
+            await self._engine.dispose()
